7eecf893eea0c7c0069303fbe5ea284b9e0bf7a7
geoff <>
2007-11-01 14:21:13 +0000

    Tidying up spelling and titles

--- include/documentation.php
+++ include/documentation.php
@@ -27,11 +27,9 @@
   <td>
    <div class="Text_Main_Header">Documentation</div>
    <div class="Text_Normal">
-   This is the documentation for the current version of Texttest, which is 3.8.
+   This is the documentation for the current version of Texttest, which is 3.9.1.
    (Find older versions here (future link))
    
-   As a beginner you should start from Installing texttest. Even if you already have 
-   it is always a good idea to double check so that you have not forgotten some details.
    </div>
    <div class="Text_Normal">  
    <table border=0 bgcolor="#000000" cellspacing=1>
@@ -40,36 +38,32 @@
           <table border=0 class="documentation_table">
             <tr>
               <td width=150 ><div class="Table_Text_Header">Tutorials</div></td>
-              <td width=230><div class="Table_Text_Header">Guides</div></td>
-              <td width=250><div class="Table_Text_Header">References</div></td>
+              <td width=230><div class="Table_Text_Header">User Guide</div></td>
+              <td width=250><div class="Table_Text_Header">Configuration Reference</div></td>
             </tr>
-            <tr>
-              <td><div class="Table_Text_Description">(beginners)</div></td>
-              <td><div class="Table_Text_Description">(advance)</div></td>
-              <td><div class="Table_Text_Description">(expert)</div></td>
-            </tr>
+            
 			<tr>
 			  <td>
 			  	<div class="Table_Text_Normal">
-			    		 <?php  printLI("install_texttest","Install Texttest"); ?>
+			    		 <?php  printLI("install_texttest","Install TextTest"); ?>
 					 <?php  printLI("getting_started","Getting Started"); ?>
 					 <?php  printLI("gui_tests","Testing a GUI"); ?>
-					 <?php  printLI("kataminesweeper","Kata Mine Sweeper"); ?>
+					 <?php  printLI("kataminesweeper","Screencast"); ?>
 				</div>
 		      </td>
 		      <td>
 		      <div class="Table_Text_Normal">
-			    	 <?php  printLI("about_testsuites","Understanding Testsuites"); ?>
-			    	 <?php  printLI("texttest_sandbox","The Texttest Sandbox"); ?>
-			    	 <?php  printLI("file_collation_and_text_filtering","File collation and Text Filtering"); ?>
-			    	 <?php  printLI("guide_to_texttest_ui","Guide to Texttest UI"); ?>
-			    	 <?php  printLI("running_texttest_unattended","Running Texttest unattended"); ?>
-			    	 <?php  printLI("faking_it_with_texttest","Faking with Texttest"); ?>
+			    	 <?php  printLI("about_testsuites","Understanding Test Suites"); ?>
+			    	 <?php  printLI("texttest_sandbox","Managing test data"); ?>
+			    	 <?php  printLI("file_collation_and_text_filtering","Configuring the textual comparison"); ?>
+			    	 <?php  printLI("guide_to_texttest_ui","Guide to TextTest UI"); ?>
+			    	 <?php  printLI("running_texttest_unattended","Running TextTest unattended"); ?>
+			    	 <?php  printLI("faking_it_with_texttest","Mocking out external programs"); ?>
 			    	 <?php  printLI("making_the_logging_configurable","Making the logging configurable"); ?>
-			    	 <?php  printLI("measuring_system_resource_usage","Measuring performance"); ?>
-			    	 <?php  printLI("automatic_failure_interpretation","Detect bugs"); ?>
-			    	 <?php  printLI("running_test_in_parallell","Running tests in parallell"); ?>
-			    	 <?php  printLI("writing_a_own_config_module","Write your own configuration"); ?>
+			    	 <?php  printLI("measuring_system_resource_usage","Performance and memory checking"); ?>
+			    	 <?php  printLI("automatic_failure_interpretation","Automatic failure interpretation"); ?>
+			    	 <?php  printLI("running_tests_in_parallel","Running tests in parallel"); ?>
+			    	 <?php  printLI("writing_a_config_module","Write your own configuration"); ?>
 			
 		      </div>
 		      </td>
