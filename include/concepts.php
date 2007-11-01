402e04efe7ef8c714b46d893a4f268be680a6f77
geoff <>
2007-11-01 15:42:24 +0000

    Fixing titles

--- include/concepts.php
+++ include/concepts.php
@@ -9,10 +9,10 @@
 <table class="Table_Normal">
  <tr>
   <td>
-   <div class="Text_Main_Header">The Concepts of Texttest: Accaptence Tests </div>		  
+   <div class="Text_Main_Header"> Acceptance Testing in theory and practice </div>		  
      <div class="Text_Normal"></div>
      <div class="Text_Normal">	
-      <A class="Text_Link" HREF="index.php?page=concepts">         <?php print($concepts); ?><I>What is Acceptence Testing?</I></A><br>
+      <A class="Text_Link" HREF="index.php?page=concepts">         <?php print($concepts); ?><I>What is Acceptance Testing?</I></A><br>
       <A class="Text_Link" HREF="index.php?page=concepts&n=why">   <?php print($why);      ?><I>Why do I need Acceptance Tests?</I></A><br>
       <A class="Text_Link" HREF="index.php?page=concepts&n=others"><?php print($others);   ?><I>Our approach compared to others</I></A><br>
    </div>
