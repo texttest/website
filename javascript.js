1f0421f99c1dbe16f42a58dec6c9d1f6038b2ed2
henning <>
2007-11-22 07:44:54 +0000

    Changed Last updated()

--- javascript.js
+++ javascript.js
@@ -1,9 +1,43 @@
-function lastUpdated() 
-{
-	var m = "Last updated: " + document.lastModified; 
-	var p = m.length-8; 
-	document.write(m.substring(p, 0)); 
+function getLongDateString()
+{	
+        //method defined on class Date.
+	//Returns a date string of the form: Day DD Month,YYYY
+	//(e.g. Sunday 27 September, 1998)
+	monthNames = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+        dayNames = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+	dayOfWeek = this.getDay();
+	day = dayNames[dayOfWeek];
+	dateOfMonth = this.getDate();
+        monthNo = this.getMonth();
+	month = monthNames[monthNo];
+        year = this.getYear();
+	if (year < 2000)
+          year = year + 1900;
+
+        dateStr = dateOfMonth+" "+month+" "+year;
+        //dateStr = day+" "+dateOfMonth+" "+month+","+year;
+	return dateStr;
 }
+//register the  method in the class Date
+Date.prototype.getLongDateString=getLongDateString;
+
+function lastUpdated()
+{ //return the document modification date (excl.time)
+//as a string
+	DateTimeStr = document.lastModified;
+	secOffset = Date.parse(DateTimeStr);
+	if (secOffset == 0 || secOffset == null) //Opera3.2
+			 dateStr = "Unknown";
+	else
+	{
+		aDate = new Date();
+		aDate.setTime(secOffset);
+		//use method defined above
+		datestr = aDate.getLongDateString();
+	}
+	document.write(dateStr);
+}
+
 function openClose(id)
 	{var obj = "";	 
  
