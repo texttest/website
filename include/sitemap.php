5339d05eb49858d01509fc5066012df02288f625
henning <>
2007-12-12 10:25:55 +0000

    New improved version

--- include/sitemap.php
+++ include/sitemap.php
@@ -2,19 +2,103 @@
 <div class="Text_Header">Site map</div>
 <table class="index">
 <div class="Text_Normal">
-<a class="Text_Link" href="index.php?page=about."><br><a class="Text_Link" href="index.php?page=concepts."><br><a class="Text_Link" href="index.php?page=contact."><br><a class="Text_Link" href="index.php?page=download."><br><a class="Text_Link" href="index.php?page=news."><br><a class="Text_Link" href="index.php?page=publications."><br><a class="Text_Link" href="index.php?page=siteinfo."><br><a class="Text_Link" href="index.php?page=sitemap."><br><a class="Text_Link" href="index.php?page=documentation."><br>concepts<br><div class="Text_Normal">
-<a class="Text_Link" href="index.php?page=appevents."><br><a class="Text_Link" href="index.php?page=concepts."><br><a class="Text_Link" href="index.php?page=shortcuts."><br><a class="Text_Link" href="index.php?page=whylog."><br><a class="Text_Link" href="index.php?page=why."><br><a class="Text_Link" href="index.php?page=xusecase."><br>CVS<br><div class="Text_Normal">
+<a class="Text_Link" href="index.php?page=main">   <div class="Text_Main_Header">Welcome to the home of Texttest</div>
+</a><br>
+<a class="Text_Link" href="index.php?page=concepts">   <div class="Text_Main_Header"> Acceptance Testing in theory and practice </div>		  
+</a><br>
+<a class="Text_Link" href="index.php?page=contact">   <div class="Text_Main_Header">Contact and support</div>
+</a><br>
+<a class="Text_Link" href="index.php?page=documentation">   <div class="Text_Main_Header">Documentation</div>
+</a><br>
+<a class="Text_Link" href="index.php?page=download">   <div class="Text_Main_Header">Download</div>
+</a><br>
+<a class="Text_Link" href="index.php?page=about">   <div class="Text_Main_Header">About Texttest</div>
+</a><br>
+<a class="Text_Link" href="index.php?page=news">   <div class="Text_Main_Header">News</div>
+</a><br>
+<a class="Text_Link" href="index.php?page=publications">a</a><br>
+<a class="Text_Link" href="index.php?page=siteinfo">   <div class="Text_Main_Header">Site info</div>
+</a><br>
+<a class="Text_Link" href="index.php?page=sitemap">a</a><br>
+<br><B>Pages under concepts</B>
+<div class="Text_Normal">
+<a class="Text_Link" href="index.php?page=concepts&n=appevents">appevents</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=calccomparison">calccomparison</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=concepts">concepts</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=guicomparison">guicomparison</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=logprobs">logprobs</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=others">others</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=problems">problems</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=pyusecase">pyusecase</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=shortcuts">shortcuts</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=webcomparison">webcomparison</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=webusecase">webusecase</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=why">why</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=whylog">whylog</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=xusecase">xusecase</a><br>
 </div>
-images<br><div class="Text_Normal">
-CVS<br><div class="Text_Normal">
+<br><B>Pages under documentation</B>
+<div class="Text_Normal">
+<a class="Text_Link" href="index.php?page=documentation&n=documentation_3_10">documentation_3_10</a><br>
+<a class="Text_Link" href="index.php?page=documentation&n=documentation_3_9_1">documentation_3_9_1</a><br>
+<a class="Text_Link" href="index.php?page=documentation&n=old_versions">old_versions</a><br>
+<br><B>Pages under documentation_3_10</B>
+<div class="Text_Normal">
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=main">main</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=automatic_failure_interpretation">automatic_failure_interpretation</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=configfile_default">configfile_default</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=configfile_queuesystem">configfile_queuesystem</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=faking_it_with_texttest">faking_it_with_texttest</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=file_collation_and_text_filtering">file_collation_and_text_filtering</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=getting_started">getting_started</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=gui_tests">gui_tests</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=guide_to_texttest_ui">guide_to_texttest_ui</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=install_texttest">install_texttest</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=kataminesweeper">kataminesweeper</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=about_testsuites">about_testsuites</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=making_the_logging_configurable">making_the_logging_configurable</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=measuring_system_resource_usage">measuring_system_resource_usage</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=options_default">options_default</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=options_queuesystem">options_queuesystem</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=queuesystem">queuesystem</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=running_tests_in_parallel">running_tests_in_parallel</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=running_texttest_unattended">running_texttest_unattended</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=scripts_default">scripts_default</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=scripts_queuesystem">scripts_queuesystem</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=texttest_sandbox">texttest_sandbox</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=writing_a_config_module">writing_a_config_module</a><br>
+</div>
+<br><B>Pages under documentation_3_9_1</B>
+<div class="Text_Normal">
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=main">main</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=automatic_failure_interpretation">automatic_failure_interpretation</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=configfile_default">configfile_default</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=configfile_queuesystem">configfile_queuesystem</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=faking_it_with_texttest">faking_it_with_texttest</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=file_collation_and_text_filtering">file_collation_and_text_filtering</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=getting_started">getting_started</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=gui_tests">gui_tests</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=guide_to_texttest_ui">guide_to_texttest_ui</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=install_texttest">install_texttest</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=kataminesweeper">kataminesweeper</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=about_testsuites">about_testsuites</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=making_the_logging_configurable">making_the_logging_configurable</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=measuring_system_resource_usage">measuring_system_resource_usage</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=options_default">options_default</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=options_queuesystem">options_queuesystem</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=queuesystem">queuesystem</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=running_tests_in_parallel">running_tests_in_parallel</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=running_texttest_unattended">running_texttest_unattended</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=scripts_default">scripts_default</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=scripts_queuesystem">scripts_queuesystem</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=texttest_sandbox">texttest_sandbox</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=writing_a_config_module">writing_a_config_module</a><br>
 </div>
 </div>
-<a class="Text_Link" href="index.php?page=others."><br><a class="Text_Link" href="index.php?page=webusecase."><br><a class="Text_Link" href="index.php?page=pyusecase."><br><a class="Text_Link" href="index.php?page=problems."><br><a class="Text_Link" href="index.php?page=logprobs."><br><a class="Text_Link" href="index.php?page=calccomparison."><br><a class="Text_Link" href="index.php?page=webcomparison."><br><a class="Text_Link" href="index.php?page=guicomparison."><br></div>
-CVS<br><div class="Text_Normal">
-</div>
-documentation<br><div class="Text_Normal">
-<a class="Text_Link" href="index.php?page=documentation_3_9_1."><br><a class="Text_Link" href="index.php?page=documentation_3_10."><br><a class="Text_Link" href="index.php?page=old_versions."><br>kataminesweeper<br><div class="Text_Normal">
-CVS<br><div class="Text_Normal">
+<br><B>Pages under publications</B>
+<div class="Text_Normal">
+<a class="Text_Link" href="index.php?page=publications&n=main">main</a><br>
+<a class="Text_Link" href="index.php?page=publications&n=bestpractices">bestpractices</a><br>
 </div>
 </div>
-documentation_3_10<br><div class="Text_Normal">
+</table></td></tr></table>
