883832b76dbcbc1274ad9808500e989d215dae27
henning <>
2007-12-13 10:47:27 +0000

    new sitemap

--- include/sitemap.php
+++ include/sitemap.php
@@ -1,104 +1,97 @@
-<table class="Table_Normal"><tr><td>
+<table width="500" class="Table_Normal"><tr><td>
 <div class="Text_Header">Site map</div>
 <table class="index">
 <div class="Text_Normal">
-<a class="Text_Link" href="index.php?page=main">   <div class="Text_Main_Header">Welcome to the home of Texttest</div>
-</a><br>
-<a class="Text_Link" href="index.php?page=concepts">   <div class="Text_Main_Header"> Acceptance Testing in theory and practice </div>		  
-</a><br>
-<a class="Text_Link" href="index.php?page=contact">   <div class="Text_Main_Header">Contact and support</div>
-</a><br>
-<a class="Text_Link" href="index.php?page=documentation">   <div class="Text_Main_Header">Documentation</div>
-</a><br>
-<a class="Text_Link" href="index.php?page=download">   <div class="Text_Main_Header">Download</div>
-</a><br>
-<a class="Text_Link" href="index.php?page=about">   <div class="Text_Main_Header">About Texttest</div>
-</a><br>
-<a class="Text_Link" href="index.php?page=news">   <div class="Text_Main_Header">News</div>
-</a><br>
-<a class="Text_Link" href="index.php?page=publications">a</a><br>
-<a class="Text_Link" href="index.php?page=siteinfo">   <div class="Text_Main_Header">Site info</div>
-</a><br>
-<a class="Text_Link" href="index.php?page=sitemap">a</a><br>
+<B>Main Pages</B><br><br>
+<a class="Text_Link" href="index.php?page=main">Welcome to the home of Texttest</a><br>
+<a class="Text_Link" href="index.php?page=concepts"> Acceptance Testing in theory and practice </a><br>
+<a class="Text_Link" href="index.php?page=contact">Contact and support</a><br>
+<a class="Text_Link" href="index.php?page=documentation">Documentation</a><br>
+<a class="Text_Link" href="index.php?page=download">Download</a><br>
+<a class="Text_Link" href="index.php?page=about">About Texttest</a><br>
+<a class="Text_Link" href="index.php?page=news">News</a><br>
+<a class="Text_Link" href="index.php?page=publications">Publications</a><br>
+<a class="Text_Link" href="index.php?page=siteinfo">Site info</a><br>
+<a class="Text_Link" href="index.php?page=sitemap">Site map (this page)</a><br>
 <br><B>Pages under concepts</B>
 <div class="Text_Normal">
-<a class="Text_Link" href="index.php?page=concepts&n=appevents">appevents</a><br>
-<a class="Text_Link" href="index.php?page=concepts&n=calccomparison">calccomparison</a><br>
-<a class="Text_Link" href="index.php?page=concepts&n=concepts">concepts</a><br>
-<a class="Text_Link" href="index.php?page=concepts&n=guicomparison">guicomparison</a><br>
-<a class="Text_Link" href="index.php?page=concepts&n=logprobs">logprobs</a><br>
-<a class="Text_Link" href="index.php?page=concepts&n=others">others</a><br>
-<a class="Text_Link" href="index.php?page=concepts&n=problems">problems</a><br>
-<a class="Text_Link" href="index.php?page=concepts&n=pyusecase">pyusecase</a><br>
-<a class="Text_Link" href="index.php?page=concepts&n=shortcuts">shortcuts</a><br>
-<a class="Text_Link" href="index.php?page=concepts&n=webcomparison">webcomparison</a><br>
-<a class="Text_Link" href="index.php?page=concepts&n=webusecase">webusecase</a><br>
-<a class="Text_Link" href="index.php?page=concepts&n=why">why</a><br>
-<a class="Text_Link" href="index.php?page=concepts&n=whylog">whylog</a><br>
-<a class="Text_Link" href="index.php?page=concepts&n=xusecase">xusecase</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=appevents">Multi-threaded simulation using Application Events</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=calccomparison">Comparing tools for acceptance testing of non-interactive calculators</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=concepts">What is Acceptence Testing?</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=guicomparison">Comparing tools for acceptance testing of custom GUIs</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=logprobs">Some Frequently Asked Questions on problems with logs as tests</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=others">Other tools for Acceptance Testing</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=problems">Frequently raised objections to Use-case Recording</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=pyusecase">Using the PyUseCase library</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=shortcuts">Test Refactoring and Macro Recording with GUI shortcuts </a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=webcomparison">Comparing tools for acceptance testing of web apps</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=webusecase">Driving a web application with WebUseCase</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=why"> Why do I need Acceptance Tests as well as Unit Tests?</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=whylog">Five good reasons to use logging for asserting behaviour in acceptance tests</a><br>
+<a class="Text_Link" href="index.php?page=concepts&n=xusecase">Using	a Use-case recorder</a><br>
 </div>
 <br><B>Pages under documentation</B>
 <div class="Text_Normal">
-<a class="Text_Link" href="index.php?page=documentation&n=documentation_3_10">documentation_3_10</a><br>
-<a class="Text_Link" href="index.php?page=documentation&n=documentation_3_9_1">documentation_3_9_1</a><br>
-<a class="Text_Link" href="index.php?page=documentation&n=old_versions">old_versions</a><br>
+<a class="Text_Link" href="index.php?page=documentation&n=documentation_3_10">Documentation for <?php echo $texttest_version ?></a><br>
+<a class="Text_Link" href="index.php?page=documentation&n=documentation_3_9_1">Documentation for <?php echo $texttest_version ?></a><br>
+<a class="Text_Link" href="index.php?page=documentation&n=old_versions">Older Versions of the Documentation</a><br>
 <br><B>Pages under documentation_3_10</B>
 <div class="Text_Normal">
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=main">main</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=automatic_failure_interpretation">automatic_failure_interpretation</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=configfile_default">configfile_default</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=configfile_queuesystem">configfile_queuesystem</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=faking_it_with_texttest">faking_it_with_texttest</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=file_collation_and_text_filtering">file_collation_and_text_filtering</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=getting_started">getting_started</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=gui_tests">gui_tests</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=guide_to_texttest_ui">guide_to_texttest_ui</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=install_texttest">install_texttest</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=kataminesweeper">kataminesweeper</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=about_testsuites">about_testsuites</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=making_the_logging_configurable">making_the_logging_configurable</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=measuring_system_resource_usage">measuring_system_resource_usage</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=options_default">options_default</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=options_queuesystem">options_queuesystem</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=queuesystem">queuesystem</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=running_tests_in_parallel">running_tests_in_parallel</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=running_texttest_unattended">running_texttest_unattended</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=scripts_default">scripts_default</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=scripts_queuesystem">scripts_queuesystem</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=texttest_sandbox">texttest_sandbox</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_10&n=writing_a_config_module">writing_a_config_module</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=main">Overview of the Documentation</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=automatic_failure_interpretation">Automatic Failure Interpretation in TextTest</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=configfile_default">List of possible entries for config files:</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=configfile_queuesystem">List of possible entries for config files</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=faking_it_with_texttest">Faking it with TextTest</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=file_collation_and_text_filtering">File Collation and Text Filtering</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=getting_started">Getting started with Texttest</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=gui_tests">Testing a GUI with TextTest and a Use Case Recorder</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=guide_to_texttest_ui">Guide to TextTest's User Interfaces</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=install_texttest">Install Texttest</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=kataminesweeper">KataMinesweeper</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=about_testsuites">Understanding TextTest Test Suites</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=making_the_logging_configurable">Making the logging configurable</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=measuring_system_resource_usage">Measuring system resource usage </a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=options_default">List of options when submitting test runs</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=options_queuesystem">List of options when submitting test runs</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=queuesystem">Tables for the queuesystem module</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=running_tests_in_parallel">Running tests in parallel on a grid</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=running_texttest_unattended">Running TextTest Unattended</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=scripts_default">Full list of provided pluginscripts</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=scripts_queuesystem">Full list of provided pluginscripts</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=texttest_sandbox">The TextTest Sandbox</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_10&n=writing_a_config_module">Using TextTest as a Python Framework: Writing your own config module</a><br>
 </div>
 <br><B>Pages under documentation_3_9_1</B>
 <div class="Text_Normal">
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=main">main</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=automatic_failure_interpretation">automatic_failure_interpretation</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=configfile_default">configfile_default</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=configfile_queuesystem">configfile_queuesystem</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=faking_it_with_texttest">faking_it_with_texttest</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=file_collation_and_text_filtering">file_collation_and_text_filtering</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=getting_started">getting_started</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=gui_tests">gui_tests</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=guide_to_texttest_ui">guide_to_texttest_ui</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=install_texttest">install_texttest</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=kataminesweeper">kataminesweeper</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=about_testsuites">about_testsuites</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=making_the_logging_configurable">making_the_logging_configurable</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=measuring_system_resource_usage">measuring_system_resource_usage</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=options_default">options_default</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=options_queuesystem">options_queuesystem</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=queuesystem">queuesystem</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=running_tests_in_parallel">running_tests_in_parallel</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=running_texttest_unattended">running_texttest_unattended</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=scripts_default">scripts_default</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=scripts_queuesystem">scripts_queuesystem</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=texttest_sandbox">texttest_sandbox</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=writing_a_config_module">writing_a_config_module</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=main">Overview of the Documentation</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=automatic_failure_interpretation">Automatic Failure Interpretation in TextTest</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=configfile_default">List of possible entries for config files:</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=configfile_queuesystem">List of possible entries for config files</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=faking_it_with_texttest">Faking it with TextTest</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=file_collation_and_text_filtering">File Collation and Text Filtering</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=getting_started">Getting started with Texttest</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=gui_tests">Testing a GUI with TextTest and a Use Case Recorder</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=guide_to_texttest_ui">Guide to TextTest's User Interfaces</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=install_texttest">Install Texttest</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=kataminesweeper">KataMinesweeper</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=about_testsuites">Understanding TextTest Test Suites</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=making_the_logging_configurable">Making the logging configurable</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=measuring_system_resource_usage">Measuring system resource usage </a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=options_default">List of options when submitting test runs</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=options_queuesystem">List of options when submitting test runs</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=queuesystem">Tables for the queuesystem module</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=running_tests_in_parallel">Running tests in parallel on a grid</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=running_texttest_unattended">Running TextTest Unattended</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=scripts_default">Full list of provided pluginscripts</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=scripts_queuesystem">Full list of provided pluginscripts</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=texttest_sandbox">The TextTest Sandbox</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_9_1&n=writing_a_config_module">Using TextTest as a Python Framework: Writing your own config module</a><br>
 </div>
 </div>
 <br><B>Pages under publications</B>
 <div class="Text_Normal">
-<a class="Text_Link" href="index.php?page=publications&n=main">main</a><br>
-<a class="Text_Link" href="index.php?page=publications&n=bestpractices">bestpractices</a><br>
+<a class="Text_Link" href="index.php?page=publications&n=main">Publications, Presentations & Events</a><br>
+<a class="Text_Link" href="index.php?page=publications&n=bestpractices"> Exploring Best Practice for XP Acceptance Testing</a><br>
 </div>
 </div>
 </table></td></tr></table>
