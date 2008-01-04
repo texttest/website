2b4e40bdc9a372426db8190a80637258c31e70ae
geoff <>
2008-01-04 11:40:48 +0000

    Fixing missing italics tag

--- include/documentation/documentation_3_9_1/file_collation_and_text_filtering.php
+++ include/documentation/documentation_3_9_1/file_collation_and_text_filtering.php
@@ -256,7 +256,7 @@ specific to this entry. This is defined as follows:
 </div>
 <UL>
 	<div class="Text_Normal"><li><SPAN STYLE="text-decoration: none"><b><I>&lt;pattern&gt;{LINES
-	&lt;number_of_lines&gt;}</b></SPAN> - On encountering a
+	&lt;number_of_lines&gt;}</I></b></SPAN> - On encountering a
 	match with &lt;pattern&gt;, instead of removing the single line
 	containing the pattern, remove &lt;number_of_lines&gt;. The
 	count includes the line matched, so {LINES 1} has no effect.</div>
