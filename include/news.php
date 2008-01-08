be4e49124192c0df0d6ba61c58f720908d39e977
henning <>
2008-01-08 11:47:44 +0000

    Added some news

--- include/news.php
+++ include/news.php
@@ -4,15 +4,17 @@
   <td>
    <div class="Text_Main_Header">News</div>
    
-   <!--
-   <div class="Text_Header">
-     14 Nov 2007
-   </div>
-   <div class="Text_Normal">
-     This site is currently on trial
-   </div>
-	-->
-  
+    <div class="Text_Header">New site for texttest released</div>
+    <div class="Text_Description">Added: 08 Jan 2008 by Henning</div>
+    <div class="Text_Normal">
+    	As you probably noticed we have released a new version of the texttest site.
+    	We have still some smaller things to fix (like the ugly background) and if you find any error or 
+    	have any suggestion of how to improve the site, 
+    	please do not hesitate to <a class="Text_Link" href="index.php?page=contact">contact</a> us. 
+    	You will find the old site <a class="Text_Link" href="old_version/index.html">here</a>
+    </div>
+
+	  
   </td>
  </tr>
 </table>
