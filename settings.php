5f2e5ba608d37f996f01af8a2eaccb20ea4f9d63
henning <>
2008-02-21 07:56:46 +0000

    Added settings.php where all global variables shoould be stored, this to make way for future automatic maintaince of the site

---
+++ settings.php
@@ -0,0 +1,16 @@
+<?php
+
+//Used to find documentation for old versions
+$pathToOldDocs="http://texttest.carmen.se/old_version/TextTest/docs";
+
+//Current release here
+$current_release = "3.10";
+
+//List of all versions
+$all_releases = array();
+array_push($all_releases, $current_release);
+
+//Older versions of the documentation found in ttwebsite ver_2
+array_push($all_releases, "3.9.1");
+
+?>
