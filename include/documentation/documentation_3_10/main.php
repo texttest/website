794d6804089b7bb935cdc52f63f5ad583ad7efb3
geoff <>
2008-05-13 13:18:56 +0000

    Extra references in the overview

--- include/documentation/documentation_3_10/main.php
+++ include/documentation/documentation_3_10/main.php
@@ -5,12 +5,12 @@ through the <a class="Text_Link" href="index.php?page=<?php echo $version ?>&n=i
 recommended that you work through one or both of the tutorials,
 this should give you a feel for basic TextTest usage. 
 </div>
-<div class="Text_Normal">The user guide material is a
-little bit more theoretical. However, it is generally useful to
-browse the first two or three documents to help gain a high
-level view of how TextTest works and how to configure it. These
-describe the basic details of building test suites and running
-tests.</div>
+<div class="Text_Normal">
+It is generally useful also to read through the material in
+the Basic Setup section, this will tell you what you need to
+know in order to get TextTest basically up and running on your
+application.
+</div>
 <div class="Text_Normal">The User Interfaces guide is
 there as a complete guide to the GUIs, which are more
 configurable than you might imagine. Read it if understanding
