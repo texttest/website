4b7c5981041a3d4be9abd0865a45c94600be0206
geoff <>
2008-05-28 06:39:46 +0000

    3.11 now the default

--- settings.php
+++ settings.php
@@ -4,14 +4,14 @@
 $pathToOldDocs="http://texttest.carmen.se/old_version/TextTest/docs";
 
 //Current release here
-$current_release = "3.10";
+$current_release = "3.11";
 
 //List of all versions
 $all_releases = array();
-array_push($all_releases, "3.11");
 array_push($all_releases, $current_release);
 
 //Older versions of the documentation found in ttwebsite ver_2
+array_push($all_releases, "3.10");
 array_push($all_releases, "3.9.1");
 
 ?>
