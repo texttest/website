d7d819b71bf88108e126303188f81fe011c1d788
henning <>
2008-06-04 10:17:23 +0000

    Addded a function called print_last_updated which locates the current body file and checks when it last was modified.

--- functions.php
+++ functions.php
@@ -12,6 +12,39 @@ function include_404_page()
 	include "404.htm";
 }
 
+function print_last_updated()
+{	
+	global $all_releases;
+	print "Last updated: ";
+	
+	$filename = 'include/main.php';
+	if (isset($_GET['page'])) 
+	{
+		$page_leads_to_docs = false;
+		foreach($all_releases as $release)
+		{
+			if (convertToDocFormat($release) == ($_GET['page']))
+			{
+				$page_leads_to_docs  = true;
+				if (isset($_GET['n'])) 
+					$filename = "include/documentation/".$_GET['page']."/".$_GET['n'].".php";
+				else 
+					$filename = "include/documentation/".$_GET['page'].".php";
+			}
+		}
+		if (!($page_leads_to_docs))
+		{	
+			if (isset($_GET['n'])) 
+				$filename = "include/".$_GET['page']."/".$_GET['n'].".php";
+			else
+				$filename = "include/".$_GET['page'].".php";
+		}
+	}
+	if (file_exists($filename)) print  date ("d F Y", filemtime($filename));
+	else print "Not available\n<!--Did not find ".$filename."--><!--FUNCTION_LAST_UPDATED_DID_NOT_WORK-->\n";
+
+}
+
 function include_file($file)
 {
 	global $all_releases;
@@ -19,7 +52,7 @@ function include_file($file)
 
 	if (isset($GLOBALS['basePath']))           global $basePath;
 	if (isset($GLOBALS['texttest_version'] ))  global $texttest_version; 
-	if (isset($GLOBALS['version']))            Global $version;
+	if (isset($GLOBALS['version']))            global $version;
 		
 	if (file_exists($file))
 		include $file;
