721735dc6de5c70aa67538b198524c9606ee3786
henning <>
2008-06-04 10:20:06 +0000

    Added some news about the new version

--- include/news.php
+++ include/news.php
@@ -3,7 +3,20 @@
  <tr>
   <td>
    <div class="Text_Main_Header">News</div>
-   
+   <div class="Text_Header">Version 3.11 is now released!</div>
+    <div class="Text_Description">Added: 27 May 2008 by Geoff</div>
+    <div class="Text_Normal">
+    	Dear all,
+	</div>
+	<div class="Text_Normal">
+		As promised TextTest 3.11 is now available for <a class="Text_Link" href="index.php?page=download">download</a>. There are lots of improvements, 
+		and a few things have moved around. I paste the whole ChangeLog below, whose formatting will 
+		no doubt get mangled by the sourceforge list software... As ever people upgrading should read 
+		the Migration Notes in the download, also available via the Help menu in the TextTest GUI.
+	</div>
+	<div class="Text_Normal">	
+		Let me know of any problems...
+    </div>
     <div class="Text_Header">New site for texttest released</div>
     <div class="Text_Description">Added: 08 Jan 2008 by Henning</div>
     <div class="Text_Normal">
@@ -13,6 +26,7 @@
     	please do not hesitate to <a class="Text_Link" href="index.php?page=contact">contact</a> us. 
     	You will find the old site <a class="Text_Link" href="old_version/index.html">here</a>
     </div>
+	
 
 	  
   </td>
