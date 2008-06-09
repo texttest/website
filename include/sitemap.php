461285cceb0baf14393d09b4180909dd914b6254
geoff <>
2008-06-09 11:06:22 +0000

    Fixing sitemap to be correct

--- include/sitemap.php
+++ include/sitemap.php
@@ -31,7 +31,7 @@
 <br><B>Pages under documentation</B>
 <div class="Text_Normal">
 <a class="Text_Link" href="index.php?page=documentation_3_10">Documentation for 3.10</a><br>
-<a class="Text_Link" href="index.php?page=documentation_3_11">Documentation for 3.11</a><br>
+<a class="Text_Link" href="index.php?page=documentation_3_11_1">Documentation for 3.11.1</a><br>
 <a class="Text_Link" href="index.php?page=documentation_3_9_1">Documentation for 3.9.1</a><br>
 <a class="Text_Link" href="index.php?page=documentation&n=old_versions">Older Versions of the Documentation</a><br>
 <br><B>Pages under documentation_3_10</B>
