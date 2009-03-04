6b70d4cc1c8e5a76649067f8042ea87eb6b64afb
Geoff Bache <geoff.bache@jeppesen.com>
2009-03-04 14:04:13 +0100

    Changed default version to 3.13

--- settings.php
+++ settings.php
@@ -4,15 +4,13 @@
 $pathToOldDocs="http://texttest.carmen.se/old_version/TextTest/docs";
 
 //Current release here
-$current_release = "3.12";
+$current_release = "3.13";
 
 //List of all versions
 $all_releases = array();
 array_push($all_releases, "trunk");
 array_push($all_releases, "3.13");
-array_push($all_releases, $current_release);
-
-//Older versions of the documentation found in ttwebsite ver_2
+array_push($all_releases, "3.12");
 array_push($all_releases, "3.11.1");
 array_push($all_releases, "3.11");
 array_push($all_releases, "3.10");
