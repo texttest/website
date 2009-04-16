6aa74f210291a9f9d6fa1290d70fe0f087831eef
Geoff Bache <geoff.bache@jeppesen.com>
2009-04-16 18:05:56 +0200

    Don't rely on self-tests for generating docs!

--- scripts/updatescripts.py
+++ scripts/updatescripts.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 
 import sys,os
+from glob import glob
 from tableshared import updateTable
 
+def makeConfigFile(configFileName, configModule):
+    configFile = open(configFileName, "w")
+    configFile.write("config_module:" + configModule + "\n")
+    configFile.close()
 
 def correctAngleBrackets(text):
     return text.replace("<", "&lt;").replace(">", "&gt;")
 
 def getScriptData(texttestPath, configModule):
     scriptRows = []
-    ttCommand = texttestPath + " -a texttest --vanilla -v sge -s default.DocumentScripts"
+    makeConfigFile("config.app", configModule)
+    ttCommand = texttestPath + " -a app -d . --vanilla -s default.DocumentScripts"
     for line in os.popen(ttCommand).readlines():
         if line.find("|") == -1:
             continue
         entries = line.strip().split("|")
         scriptRows.append(entries)
+    for fileName in glob("*.app"):
+        os.remove(fileName)
     return scriptRows
 
 origTable = sys.argv[1]
