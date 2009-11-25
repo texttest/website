dbc2ce4b5de04915c03dbbdbe98de3accbd7ceb1
Geoff Bache <geoff.bache@jeppesen.com>
2009-11-25 21:31:14 +0100

    Updated abstract descriptions

--- include/ui_testing/appevents.php
+++ include/ui_testing/appevents.php
@@ -1,77 +1,84 @@
-<div class="Text_Header">Multi-threaded simulation using Application Events</div>
+<div class="Text_Header">Multi-threaded simulation using Application Events</div>				
+<div class="Text_Description">Allowing recording and replaying of waits</div>				
+<div class="Text_Normal">A typical GUI application has the requirement of instant
+  response to its user at all times. It cannot just lock up while
+  some background processing (e.g. loading in a large amount of
+  data) is done. This means that a large number of GUIs have
+  multiple threads, and any acceptance testing approach for GUIs
+  must therefore consider how to handle this problem.</div>
 
-				
-				
-					<div class="Text_Normal">A typical GUI application has the requirement of instant
-					response to its user at all times. It cannot just lock up while
-					some background processing (e.g. loading in a large amount of
-					data) is done. This means that a large number of GUIs have
-					multiple threads, and any acceptance testing approach for GUIs
-					must therefore consider how to handle this problem.</div>
-
-					<div class="Text_Normal">When we replay a test without human intervention, it may well
-					be necessary to wait for things to happen before proceeding.
-					Otherwise the test will fail because further use case actions
-					rely on data loaded in a separate thread being present. In this
-					case a traditional record/replay tool is basically stuck: it
-					knows nothing of application intent and all it can do is ask the
-					test writer to hand-insert 'sleep' statements into the script
-					after recording it. Needless to say, this is both inefficient
-					and error-prone: it can easily be forgotten, and the required
-					length of sleep is hard to get right. If the test is being
-					written by a customer unaware of the thread structure of the
-					program (as we intend), these problems are of course further
-					exacerbated.</div>
-					<div class="Text_Normal">Use-case recorders handle this situation by introducing the
-					notion of an `application event': the application can simply
-					notify the use-case recorder when a significant event has
-					occurred that is worth waiting for. At places in the code where
-					such events occur, the programmer adds calls to xUseCase, which
-					will then record a `wait
-					for &lt;name of application event&gt;' command.
-					During replay the replay thread will halt until the application
-					reaches the point where the application event occurs, i.e., when
-					the use-case recorder is notified of the event having occurred.</div>
-					<div class="Text_Normal">So, xUseCase recording with application events looks like
-					this:</div>
-					<div class="Text_Normal"><img src="include/concepts/images/xusecaserec.jpg" NAME="Graphic1" ALIGN=LEFT WIDTH=500 HEIGHT=152 BORDER=0><BR CLEAR=LEFT>Replaying,
+<div class="Text_Normal">When we replay a test without human intervention, it may well
+  be necessary to wait for things to happen before proceeding.
+  Otherwise the test will fail because further use case actions
+  rely on data loaded in a separate thread being present. In this
+  case a traditional record/replay tool is basically stuck: it
+  knows nothing of application intent and all it can do is ask the
+  test writer to hand-insert 'sleep' statements into the script
+  after recording it. Needless to say, this is both inefficient
+  and error-prone: it can easily be forgotten, and the required
+  length of sleep is hard to get right. 
+</div>
+<div class="Text_Normal">Use-case recorders handle this situation by introducing the
+  notion of an `application event': the application can simply
+  notify the use-case recorder when a significant event has
+  occurred that is worth waiting for. At places in the code where
+  such events occur, the programmer adds calls to xUseCase, which
+  will then record a `wait
+  for &lt;name of application event&gt;' command to the usecase it is recording.
+  During replay it will simply not replay any further events until the application
+  reaches the same point, i.e. when the same call is made. A single simple call
+  can therefore be used for both purposes.</div>
+<div class="Text_Normal">So, xUseCase recording with application events looks like
+  this:</div>
+<div class="Text_Normal"><img src="include/concepts/images/xusecaserec.jpg" NAME="Graphic1" ALIGN=LEFT WIDTH=500 HEIGHT=152 BORDER=0><BR CLEAR=LEFT>Replaying,
 					with application events, looks like this:
-					<img src="include/concepts/images/xusecaserep.jpg" NAME="Graphic4" ALIGN=LEFT WIDTH=498 HEIGHT=110 BORDER=0><BR CLEAR=LEFT>
+  <img src="include/concepts/images/xusecaserep.jpg" NAME="Graphic4" ALIGN=LEFT WIDTH=498 HEIGHT=110 BORDER=0><BR CLEAR=LEFT>
   
-					</div><HR>
-					<div class="Text_Normal">Now for an example. Assume we have the following use case
-					script from a Swing App, using JUseCase:</div><div class="Text_Normal">
-					<?php codeSampleBegin(); ?><I>load movie data into list</I>
-<I>select movie Die Hard</I><?php codeSampleEnd(); ?></div><div class="Text_Normal">
-					Also assume that the first command starts a separate thread that
-					loads a large amount of data from a database and displays it on
-					the screen. Unless there is a way of telling the replayer when
-					this has completed, it would perhaps try to select `Die Hard'
-					before that item was present in the list, causing the simulation
-					to fail. To solve this, the programmer inserts the statement</div>
-					<div class="Text_Normal"><I>ScriptEngine.instance().applicationEvent('data to be
-					loaded');</I></div>
-					<div class="Text_Normal">in his application at the point directly after the loading is
-					completed. The recorded use case will now look like this:</div>
-					<div class="Text_Normal">
-					<?php codeSampleBegin(); ?>
+</div><HR>
+<div class="Text_Normal">Now for an example. Assume we have the following use case
+  script:</div>
+<div class="Text_Normal">
+<?php codeSampleBegin(); ?>
+<I>load movie data into list</I>
+<I>select movie Die Hard</I>
+<?php codeSampleEnd(); ?>
+</div>
+<div class="Text_Normal">
+  Also assume that the first command starts a separate thread that
+  loads a large amount of data from a database and displays it on
+  the screen. Unless there is a way of telling the replayer when
+  this has completed, it would perhaps try to select `Die Hard'
+  before that item was present in the list, causing the simulation
+  to fail. To solve this, the programmer finds the point in his application
+  directly after the loading is completed and inserts the following code :
+<?php codeSampleBegin(); ?>
+import usecase
+usecase.applicationEvent('data to be loaded')
+<?php codeSampleEnd(); ?>
+if he writing a Python GUI and using PyUseCase or
+<?php codeSampleBegin(); ?>
+ScriptEngine.instance().applicationEvent('data to be loaded');
+<?php codeSampleEnd(); ?>
+if he is writing a Java GUI and using JUseCase. Either way, the recorded use case will now look like this:</div>
+<div class="Text_Normal">
+<?php codeSampleBegin(); ?>
 <I>load movie data into list</I>
 <I>wait for data to be loaded</I>
 <I>select movie Die Hard</I>
 <?php codeSampleEnd(); ?>
-					</div>
-					<div class="Text_Normal">
-					In record mode the applicationEvent method just records the
-					'wait for' command to the script file when it is called, as
-					shown here: 
-					</div>
-					<div class="Text_Normal"><BR><BR>
-					</div>
-					<div class="Text_Normal"><img src="include/concepts/images/appeventrec.jpg" NAME="Graphic3" ALIGN=LEFT WIDTH=500 HEIGHT=205 BORDER=0><BR CLEAR=LEFT>In
-					replay mode, the simulator (or replayer) halts replaying on
-					reading this 'wait for' command, and the applicationEvent call
-					then acts as a notifier to tell it to resume when the data has
-					been loaded.</div>
+</div>
+<div class="Text_Normal">
+  In record mode the applicationEvent method just records the
+  'wait for' command to the script file when it is called, as
+  shown here: 
+</div>
+<div class="Text_Normal"><BR><BR>
+</div>
+<div class="Text_Normal"><img src="include/concepts/images/appeventrec.jpg" NAME="Graphic3" ALIGN=LEFT WIDTH=500 HEIGHT=205 BORDER=0><BR CLEAR=LEFT>In
+  replay mode, the simulator (or replayer) halts replaying on
+  reading this 'wait for' command, and the applicationEvent call
+  then acts as a notifier to tell it to resume when the data has
+  been loaded.</div>
 
-					<div class="Text_Normal"><img src="include/concepts/images/appeventrep.JPG" NAME="Graphic5" ALIGN=LEFT WIDTH=495 HEIGHT=249 BORDER=0><BR CLEAR=LEFT><BR>
-					</div>
+<div class="Text_Normal"><img src="include/concepts/images/appeventrep.JPG" NAME="Graphic5" ALIGN=LEFT WIDTH=495 HEIGHT=249 BORDER=0><BR CLEAR=LEFT><BR>
+</div>
--- include/ui_testing/xusecase.php
+++ include/ui_testing/xusecase.php
@@ -1,41 +1,114 @@
-<div class="Text_Header">Using	a Use-case recorder</div> 
-<div class="Text_Description">to drive a GUI or web application</div>
-					<div class="Text_Normal">We are of the opinion that acceptance tests should model the
-					actions of a real user of the system as closely as possible.
-					Where there is a user interface (custom or web), it should be
-					present for the tests too in order to catch as many bugs as
-					possible, not bypassed as is common in many API-driven
-					approaches. 
-					</div>
-					<div class="Text_Normal">Recording a test 'live' by using the system under test in
-					some way is a simple and fast way to create such tests. It is,
-					however, a terrible way to maintain tests &ndash; constantly
-					re-recording a test is error-prone, frustrating and very
-					time-consuming once you have a few tests. We aim, therefore, to
-					provide an agile version of record/playback which records a
-					high-level script in the language of the domain. This will allow
-					increased understanding, less dependence on external factors and
-					easier adjustment of existing tests without resorting to
-					clicking all the buttons again.</div>
-					<div class="Text_Normal">Enter the use-case recorder. This concept is intended to
-					indicate a recorder that can record what is <I>intended</I> when
-					a user performs some action on a GUI widget. This is in contrast
-					to where he clicked on the screen, what the exact type of the
-					widget he used was or exactly what sequence of keys he pressed
-					to enter a text string.</div>
-
-					<div class="Text_Normal">This is achieved (in a custom GUI) by providing a
-					record/playback library that can be called by the system under
-					test, to associate each relevant GUI event with a 'use case
-					command' which can be any text string but which should describe
-					what is intended in terms of the domain - and should avoid any
-					mention of how it is achieved. In 'record' mode this library can
-					then simply add an extra listener to the event which will record
-					the string provided in a 'use case' , as illustrated:</div>
-					<div class="Text_Normal"><img src="include/concepts/images/xusecaserecnoapp.JPG" NAME="Graphic1" ALIGN=LEFT WIDTH=500 HEIGHT=152 BORDER=0><BR CLEAR=LEFT>In
-					'replay' mode, the xUseCase library will read the recorded use
-					case file, and on encountering the string associated with the
-					event, the library can 'simulate the event':</div>
-					<div class="Text_Normal"><img src="include/concepts/images/xusecaserepnoapp.JPG" NAME="Graphic4" ALIGN=LEFT WIDTH=497 HEIGHT=60 BORDER=0><BR CLEAR=LEFT><BR>
-					</div>
-				
+<div class="Text_Header">Maintainable GUI testing with a Use Case Recorder</div> 
+<div class="Text_Description">Recording the intent rather than the mechanics</div>
+<div class="Text_Normal">
+  The most natural way to create tests via a user interface is
+  to simply carry out the actions you wish to perform and have a tool
+  that can record them and then reproduce them later. This
+  is a simple and fast way to create GUI tests and there exist 
+  many tools that do this.
+</div>
+<div class="Text_Header">Most tools couple the tests tightly to the GUI</div> 
+<div class="Text_Normal">
+  The problems start when you have a few tests and your GUI changes.
+  Recording may be a great way to create tests but it's a terrible way 
+  to maintain large numbers of them. It is error-prone, frustrating and very
+  time-consuming once you have a few tests. The first generation of
+  tools recorded pixel positions and broke as soon as you changed your
+  screen resolution. Today's tools deal in terms of the GUI mechanics:
+  find a table with a certain name and click in the third column of the
+  fourth row. They can survive screen changes and minor re-arrangements of the GUI but
+  not much else. The recorded scripts are dense and don't convey the
+  purpose of the test, and are a closed book to all non-technical people (and sometimes
+  to everyone except the author of the tool).
+</div>
+<div class="Text_Normal">
+  The problem is essentially one of coupling. The tests and the GUI are
+  tightly coupled to each other and cannot comfortably vary independently
+  of one another. This point is made well by Robert C. Martin in his blog
+  <A class="Text_Link" href="http://blog.objectmentor.com/articles/2009/09/29/ruining-your-test-automation-strategy">
+  here</A> and his conclusion is that GUI testing is inherently brittle and
+  you should do as little of it as you can get away with. 
+</div>
+<div class="Text_Normal">
+  This seems rather defeatist though. There is huge value in being
+  able to demonstrate what your tests do to a user of the system.
+  If the tests bypass the user interface then that process demands
+  a fair amount of technical skill and a lot of trust from the part
+  of your user. And anyway, software developers solve coupling problems all the time.
+  The answer is, as usual, to introduce another level of indirection.
+</div>
+<div class="Text_Header">Breaking the coupling with a UI map</div> 
+<div class="Text_Normal">
+  Business people and users generally work in <i>use cases</i>. These
+  are high-level descriptions of a sequence of actions in a language
+  they understand: i.e. that of the domain. The idea of a "Use Case Recorder" is
+  therefore a tool that can record and replay such sequences and thereby
+  capture the <I>intent</I> of the user. This will then allow
+  increased understanding, less dependence on the exact form of the GUI and
+  easier adjustment of existing tests without resorting to
+  clicking all the buttons again.
+</div>
+<div class="Text_Normal">
+  The basic mechanism is that we maintain a mapping between the actions
+  that can currently be performed with our GUI and statements in this domain
+  language. GUI changes then mean that this single mapping needs to be updated,
+  but the tests can remain untouched, continuing to describe what needs
+  to be done on the conceptual level. This mapping takes the form of an
+  external file in PyUseCase 3.0 and the forthcoming JUseCase 3.0, while
+  in older versions it takes the form of instrumentation in the application 
+  code.
+</div>
+<div class="Text_Header">Checking the behaviour via logs and TextTest</div>
+<div class="Text_Normal">
+  So our use-case recorder can record and replay usecases for us. But how
+  can we check that what we see on the screen is correct? Most GUI tools
+  do this by allowing the test script to contain "assertions", which look
+  up some widget and check that some property of it is equal to a hardcoded value.
+  This creates yet more dependence on the current GUI layout and cannot be
+  "recorded" in any natural way, but has to be programmed in after the fact.
+  No "usecase" would naturally contain this information : if it did it would
+  turn into a test script.
+</div>
+<div class="Text_Normal">
+  This discussion isn't on the TextTest site for nothing. If we can only get
+  our application to produce a log of what the GUI looks like we can check
+  what it does by monitoring the contents of that log using TextTest. PyUseCase 3.0 does
+  this for you: it generates an ASCII-art type log of the current GUI appearance and monitors
+  changes to it. The application can supplement it with its own logging as it wishes. With 
+  other use-case recorders the application needs to build its own log for this purpose
+  currently.
+</div>
+<div class="Text_Header">Synchronising tests by code instrumentation</div>
+<div class="Text_Normal">
+  Almost all GUI testing efforts are plagued by problems with making
+  sure the script waits long enough before proceeding when something
+  happens in the background. The solutions range from arcane ways
+  to wait for some widget to have a certain appearance (yet more dependencies on 
+  GUI-mechanics) to "sleep" statements liberally scattered around.
+  Which fail when the system is loaded and cause the tests to run much
+  more slowly than they otherwise would. Anyone without intimate knowledge
+  of the code is ill-equipped to solve such problems, yet doing so is
+  a vital part of writing tests.
+</div>
+<div class="Text_Normal">
+  Use-case Recorders introduce the concept of an <b>"Application Event"</b>. This
+  is basically some instrumentation in the code that indicates to the use-case
+  recorder that something has happened that needs to be waited for, thus allowing
+  the recorder to record and replay waits as well as clicks. These are described
+  in more detail <a href="index.php?page=ui_testing&n=appevents" class="Text_Link">here</A>.
+</div>
+<div class="Text_Header">Recording macros as well as tests</div>
+<div class="Text_Normal">
+  High-level, easily manipulated "usecases" are useful for other things than
+  testing. They are also extremely useful for users of the system who can
+  create their own macros for sequences of actions they perform frequently.
+</div>
+<div class="Text_Normal">
+  These are known as <b>"GUI shortcuts"</b> here. A Use-case recorder will typically
+  allow an application to request a "toolbar" from it which contains controls
+  for recording and replaying them which can be inserted into the application GUI as desired.
+  Besides allowing users to create macros, they can also be used to create even higher level abstractions for
+  the "test language" described above, aiding testers in performing repeated actions
+  to reach a certain screen for testing. These are described in more detail 
+  <a href="index.php?page=ui_testing&n=shortcuts" class="Text_Link">here</A>.
+</div>
