ff1ec7e3224eb1cd27c899cd16ce0f49784dc18a
Geoff Bache <geoff.bache@jeppesen.com>
2009-11-30 21:42:59 +0100

    Improving documentation of shortcuts and receiving signals

--- include/ui_testing/pyusecase_additional.php
+++ include/ui_testing/pyusecase_additional.php
@@ -59,19 +59,38 @@ you insert a "sleep" using PyUseCase if there is nothing else you can do.
 <div class="Text_Header">Using the shortcut mechanism</div>
 <div class="Text_Normal">If you want to make <a class="Text_Link" href="index.php?page=ui_testing&n=shortcuts">shortcuts</A>
 available to your users and test writers, the basic mechanism is to import the "gtkusecase"
-module and call the "createShortcutBar" method, which will
-return a gtk.HBox. This is, by convention, added to a GUI at the
+module and call the "createShortcutBar" method. The signature of this method is as follows:
+<?php codeSampleBegin() ?>
+def createShortcutBar(uiMapFiles=[])
+<?php codeSampleEnd() ?>
+Although the uiMapFiles parameter defaults to empty, which means the default location will
+be used ($USECASE_HOME/ui_map.conf) it is usually a good idea to fill in this parameter
+with some path under the source tree of your application. After all, if you're using shortcuts
+you want ordinary users to be able to take advantage of PyUseCase functionality without
+explicitly running PyUseCase on the command line, so it makes sense for the UI map file(s) to
+live with your source code and be distributed with it. You can also provide multiple file names,
+which helps separate out the UI mapping for different parts of your application and allow it to 
+only load the currently relevant parts.
+</div>
+<div class="Text_Normal">
+The gtkusecase.createShortcutBar method will return a gtk.HBox. 
+This is, by convention, added to a GUI at the
 bottom. It will contain buttons for creating a new shortcut, and
 for all existing shortcuts found.</div>
 <div class="Text_Normal">Such shortcuts will be recorded as files in the directory
 indicated by the environment variable USECASE_HOME (defaulting
 to ~/usecases on UNIX). Also, where a user makes a sequence of
 clicks which correspond to an existing shortcut, this will be
-recorded as the shortcut name.</div>
+recorded as the shortcut name. This helps to keep shortcuts
+concise and avoid duplication. Naturally this is the case irrespective
+of whether it's being done for testing or macro creation purposes. 
+</div>
 <div class="Text_Normal">To see this in action, try out the video store example that
-comes with the PyUseCase download.</div>
+comes with the PyUseCase download, which has this call in it and hence comes with
+a shortcut bar.
+</div>
 
-<div class="Text_Header">PyUseCase and console applications</div>
+<div class="Text_Header">Recording received signals in console applications</div>
 <div class="Text_Normal">Unlike <a class="Text_Link" href="http://jusecase.sourceforge.net">JUseCase</A> which has only
 been used on GUIs, PyUseCase has also been used to provide
 simulation around console applications on UNIX (mainly <a class="Text_Link" href="index.php?page=about">TextTest</A>
