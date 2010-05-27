0e782ba188bf2573a1e6677df537a1017b36eced
Geoff Bache <geoff.bache@jeppesen.com>
2010-05-27 21:51:02 +0200

    Updating supported widgets for new 3.3 release

--- include/ui_testing/pyusecase_supported_gtk.php
+++ include/ui_testing/pyusecase_supported_gtk.php
@@ -1,6 +1,6 @@
 <div class="Text_Header">PyGTK Widgets and signals supported for record/replay</div>
 <div class="Text_Normal">The following lists the PyGTK widget types and the associated signals on them which 
-PyUseCase 3.2 is currently capable of recording and replaying. Any type derived from the listed
+PyUseCase 3.3 is currently capable of recording and replaying. Any type derived from the listed
 types is also supported.
 </div>
 <div class="Text_Normal"><table border=1 cellpadding=1 cellspacing=1>
@@ -15,6 +15,7 @@ types is also supported.
 <tr><td><a class="Text_Link" href=http://library.gnome.org/devel/pygtk/stable/class-gtkmenuitem.html>gtk.MenuItem</a></td><td><div class="Table_Text_Normal">activate</div></td></tr>
 <tr><td><a class="Text_Link" href=http://library.gnome.org/devel/pygtk/stable/class-gtknotebook.html>gtk.Notebook</a></td><td><div class="Table_Text_Normal">switch-page</div></td></tr>
 <tr><td><a class="Text_Link" href=http://library.gnome.org/devel/pygtk/stable/class-gtkpaned.html>gtk.Paned</a></td><td><div class="Table_Text_Normal">notify::position</div></td></tr>
+<tr><td><a class="Text_Link" href=http://library.gnome.org/devel/pygtk/stable/class-gtktextview.html>gtk.TextView</a></td><td><div class="Table_Text_Normal">changed</div></td></tr>
 <tr><td><a class="Text_Link" href=http://library.gnome.org/devel/pygtk/stable/class-gtktogglebutton.html>gtk.ToggleButton</a></td><td><div class="Table_Text_Normal">toggled</div></td></tr>
 <tr><td><a class="Text_Link" href=http://library.gnome.org/devel/pygtk/stable/class-gtktoggletoolbutton.html>gtk.ToggleToolButton</a></td><td><div class="Table_Text_Normal">toggled</div></td></tr>
 <tr><td><a class="Text_Link" href=http://library.gnome.org/devel/pygtk/stable/class-gtktoolbutton.html>gtk.ToolButton</a></td><td><div class="Table_Text_Normal">clicked</div></td></tr>
@@ -25,7 +26,7 @@ types is also supported.
 </table></div>
 <div class="Text_Header">PyGTK Widgets supported for automatic logging</div>
 <div class="Text_Normal">
-The following lists the PyGTK widget types whose status and changes PyUseCase 3.2 is 
+The following lists the PyGTK widget types whose status and changes PyUseCase 3.3 is 
 currently capable of monitoring and logging. Any type derived from the listed types 
 is also supported but will only have features of the listed type described.
 </div>
@@ -49,6 +50,7 @@ is also supported but will only have features of the listed type described.
 <li><a class="Text_Link" href=http://library.gnome.org/devel/pygtk/stable/class-gtkseparator.html>gtk.Separator</a>
 <li><a class="Text_Link" href=http://library.gnome.org/devel/pygtk/stable/class-gtkseparatormenuitem.html>gtk.SeparatorMenuItem</a>
 <li><a class="Text_Link" href=http://library.gnome.org/devel/pygtk/stable/class-gtkseparatortoolitem.html>gtk.SeparatorToolItem</a>
+<li><a class="Text_Link" href=http://library.gnome.org/devel/pygtk/stable/class-gtkspinbutton.html>gtk.SpinButton</a>
 <li><a class="Text_Link" href=http://library.gnome.org/devel/pygtk/stable/class-gtktable.html>gtk.Table</a>
 <li><a class="Text_Link" href=http://library.gnome.org/devel/pygtk/stable/class-gtktextview.html>gtk.TextView</a>
 <li><a class="Text_Link" href=http://library.gnome.org/devel/pygtk/stable/class-gtktogglebutton.html>gtk.ToggleButton</a>
--- include/ui_testing/pyusecase_supported_tkinter.php
+++ include/ui_testing/pyusecase_supported_tkinter.php
@@ -1,29 +1,36 @@
 <div class="Text_Header">Tkinter Widgets and actions supported for record/replay</div>
 <div class="Text_Normal">The following lists the Tkinter widget types and the associated actions on them which 
-PyUseCase 3.2 is currently capable of recording and replaying. Any type derived from the listed
-types is also supported. (Note that tkMessageBox, while not directly supported by PyUseCase, can be handled
-fairly painlessly by using "collect_traffic_py_module" in TextTest)
+PyUseCase 3.3 is currently capable of recording and replaying. Any type derived from the listed
+types is also supported.
 </div>
 <div class="Text_Normal"><table border=1 cellpadding=1 cellspacing=1>
 <tr><td><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/button.html>Tkinter.Button</a></td><td><div class="Table_Text_Normal">clicked</div></td></tr>
+<tr><td><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/canvas.html>Tkinter.Canvas</a></td><td><div class="Table_Text_Normal">left-clicked , middle-clicked , right-clicked</div></td></tr>
+<tr><td><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/checkbutton.html>Tkinter.Checkbutton</a></td><td><div class="Table_Text_Normal">checked , unchecked</div></td></tr>
 <tr><td><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/entry.html>Tkinter.Entry</a></td><td><div class="Table_Text_Normal">edited text</div></td></tr>
 <tr><td><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/label.html>Tkinter.Label</a></td><td><div class="Table_Text_Normal">left-clicked , middle-clicked , right-clicked</div></td></tr>
+<tr><td><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/listbox.html>Tkinter.Listbox</a></td><td><div class="Table_Text_Normal">select item</div></td></tr>
 <tr><td><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/menu.html>Tkinter.Menu</a></td><td><div class="Table_Text_Normal">select item</div></td></tr>
 <tr><td><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/tk.html>Tkinter.Tk</a></td><td><div class="Table_Text_Normal">closed</div></td></tr>
 <tr><td><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/toplevel.html>Tkinter.Toplevel</a></td><td><div class="Table_Text_Normal">closed</div></td></tr>
 </table></div>
 <div class="Text_Header">Tkinter Widgets supported for automatic logging</div>
 <div class="Text_Normal">
-The following lists the Tkinter widget types whose status and changes PyUseCase 3.2 is 
+The following lists the Tkinter widget types whose status and changes PyUseCase 3.3 is 
 currently capable of monitoring and logging. Any type derived from the listed types 
 is also supported but will only have features of the listed type described.
 </div>
 <div class="Text_Normal">
 <li><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/button.html>Tkinter.Button</a>
+<li><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/canvas.html>Tkinter.Canvas</a>
+<li><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/checkbutton.html>Tkinter.Checkbutton</a>
 <li><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/entry.html>Tkinter.Entry</a>
 <li><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/frame.html>Tkinter.Frame</a>
 <li><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/label.html>Tkinter.Label</a>
+<li><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/labelframe.html>Tkinter.LabelFrame</a>
+<li><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/listbox.html>Tkinter.Listbox</a>
 <li><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/menu.html>Tkinter.Menu</a>
+<li><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/menubutton.html>Tkinter.Menubutton</a>
 <li><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/scrollbar.html>Tkinter.Scrollbar</a>
 <li><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/text.html>Tkinter.Text</a>
 <li><a class="Text_Link" href=http://infohost.nmt.edu/tcc/help/pubs/tkinter/tk.html>Tkinter.Tk</a>
