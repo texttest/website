07404857b3ddbd920fd1ff541e1dce3a45a57180
Geoff Bache <geoff.bache@jeppesen.com>
2010-09-27 22:20:50 +0200

    Fixing typo

--- include/ui_testing/pyusecase_friendly_coding_wx.php
+++ include/ui_testing/pyusecase_friendly_coding_wx.php
@@ -6,10 +6,10 @@ wxPython support is currently very much alpha status and isn't really mature eno
 <div class="Text_Normal">
 As discussed elsewhere, PyUseCase identifies widgets by Name, Title, Label and Type, in that order of preference. Many widgets simply don't have a Title or a Label attached to them and hence if you don't set names on them, they will be identified by Type, which will not work if you have more than one of them. Besides this, widget titles and labels may not be unique, or they may change depending on e.g. today's date. It's fair to say that almost every non-trivial application is going to need to set at least some widget names before PyUseCase will work smoothely.</div>
 <div class="Text_Normal">
-Widget naming in wxPython is a matter of calling the SetName method
+Widget naming in wxPython is a matter of calling the SetName method:
 <?php codeSampleBegin() ?>
-textCtrl = wx.Button(frame, 1, "Hello")
-textCtrl.SetName("hello world button")
+button = wx.Button(frame, 1, "Hello")
+button.SetName("hello world button")
 <?php codeSampleEnd() ?>
 </div>
 </div>
