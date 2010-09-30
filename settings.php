e4a89bbb0b6f9d9a4c084f2452e6eb91c8552d17
Geoff Bache <geoff.bache@jeppesen.com>
2010-09-30 16:38:53 +0200

    Added 3.20, removed 3.11

--- settings.php
+++ settings.php
@@ -4,11 +4,12 @@
 $pathToOldDocs="http://texttest.carmen.se/old_version/TextTest/docs";
 
 //Current release here
-$current_release = "3.19";
+$current_release = "3.20";
 
 //List of all versions
 $all_releases = array();
 array_push($all_releases, "trunk");
+array_push($all_releases, "3.20");
 array_push($all_releases, "3.19");
 array_push($all_releases, "3.18");
 array_push($all_releases, "3.17");
@@ -18,7 +19,6 @@ array_push($all_releases, "3.14");
 array_push($all_releases, "3.13");
 array_push($all_releases, "3.12");
 array_push($all_releases, "3.11.1");
-array_push($all_releases, "3.11");
 array_push($all_releases, "3.10");
 array_push($all_releases, "3.9.1");
         
