11e575faada7b72308cd9d049f0576decacf8c75
Geoff Bache <geoff.bache@jeppesen.com>
2011-11-13 12:27:27 +0100

    Updating documentation to version 3.22

--- settings.php
+++ settings.php
@@ -4,11 +4,12 @@
 $pathToOldDocs="http://texttest.carmen.se/old_version/TextTest/docs";
 
 //Current release here
-$current_release = "3.21";
+$current_release = "3.22";
 
 //List of all versions
 $all_releases = array();
 array_push($all_releases, "trunk");
+array_push($all_releases, "3.22");
 array_push($all_releases, "3.21");
 array_push($all_releases, "3.20");
 array_push($all_releases, "3.19");
@@ -18,9 +19,9 @@ array_push($all_releases, "3.16");
 array_push($all_releases, "3.15");
 array_push($all_releases, "3.14");
 array_push($all_releases, "3.13");
-array_push($all_releases, "3.12");
 $all_releases_direct_links = $all_releases;
 
+array_push($all_releases, "3.12");
 array_push($all_releases, "3.11.1");
 array_push($all_releases, "3.10");
 array_push($all_releases, "3.9.1");
