a657a86811357869fdf79df9b222e65a96506f92
Geoff Bache <geoff.bache@jeppesen.com>
2013-02-07 13:41:33 +0100

    Updating contact details

--- include/contact.php
+++ include/contact.php
@@ -6,7 +6,7 @@
    
    <div class="Text_Normal">
      
-     <p>All TextTest support issues should go via the <A class="Text_Link" HREF="https://lists.sourceforge.net/lists/listinfo/texttest-users">texttest-users mailing list</A></p>. 
+     <p>All TextTest support issues should go via the <A class="Text_Link" HREF="https://lists.sourceforge.net/lists/listinfo/texttest-users">texttest-users mailing list</A>.</p>
      <p>The bugtracker can be found at <A class="Text_Link" HREF="https://bugs.launchpad.net/texttest">Launchpad</A></p>
      <p>The developers are Geoff Bache and Daniel Galdames. In both cases "firstname.lastname@jeppesen.com" will reach us.</p>
    </div>
